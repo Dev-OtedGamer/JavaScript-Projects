076795b55db9a63d0e00ce5d6eeaafc0508b0215
Joshua Rollins <159722108+Dev-OtedGamer@users.noreply.github.com>
2024-05-25 11:54:28 -0700

    added to Basic JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+window.alert("Hello, World!"); // a pop up alert window
+document.write("Hello, World! "); // prints to browser
+
+var D = "'Dog' is a string. "; // Sets values to variables
+document.write(D); // prints variables
+var C = "'Cat' is a string assigned to the variable 'C' ."; // Sets values to variables
+document.write(C); // prints variables
+
+document.write(" Here\'s my, \"Escape Character Assignment\". "); // using "\" to show special characters
+
+document.write("\"There is always a method,"
+    + " to my madness, even though you don\'t quite see it yet!\""
+    + "-Shakespeare\" \\ with a little \"Dev-Oted\" thrown in."
+); // used the Esacpe Character to show special characters with in a concatenated statement
+
+var S = "Here\'s my" + "\"Concatenated String Challenge\"";// This sets a concatenated to a varaible
+var T =" set to the variable \"S\" & \"T\".";  // This sets a String to a variable
+document.write(S + T); // prints  the variable assigned to the string and concatenated statements
+
+var Venue = "The lineup", Third = "Spammer Jay", First = "Top Dog", Second = "Tap Out",
+    Forth = "Deceiver"; // sets multiple Variables in one Statement 
+document.write(First); // prints a variable
+
+"Tickets cost", 8.0 + 8.0 // Expression
